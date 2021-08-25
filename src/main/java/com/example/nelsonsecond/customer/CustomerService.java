f397829d027c83d6f118720121ea2beaeab3d86d
Dariusz Okonski <darek200180@gmail.com>
2021-08-25 12:29:13 +0100

    add interface CustomerFakeRepository

--- src/main/java/com/example/nelsonsecond/customer/CustomerService.java
+++ src/main/java/com/example/nelsonsecond/customer/CustomerService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-
     public Customer getCustomer() {
         return new Customer(1L, "James Bond 007");
     }
