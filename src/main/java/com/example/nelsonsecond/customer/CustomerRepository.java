f2274b999ab785829d775a106188a39d2e4c2586
Dariusz Okonski <darek200180@gmail.com>
2021-08-25 13:09:55 +0100

    add CustomerConfiguration

--- src/main/java/com/example/nelsonsecond/customer/CustomerRepository.java
+++ src/main/java/com/example/nelsonsecond/customer/CustomerRepository.java
@@ -1,15 +1,11 @@
 package com.example.nelsonsecond.customer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
-@Primary
+@Repository
 public class CustomerRepository implements CustomerRepo{
 
     @Override
